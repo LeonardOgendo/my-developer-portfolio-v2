48e11e97fd240920834e1dc2804239a71a0cfff6
Leonard Ogendo <ogendoleonard5@gmail.com>
2025-08-03 15:37:10 +0300

    Update Readme.md

--- src/shared/utils/dateUtils.jsx
+++
@@ -1,9 +0,0 @@
-export const calculateMonths = (startDate, endDate = new Date()) => {
-    const start = new Date(startDate);
-    const end = new Date(endDate);
-
-    const years = end.getFullYear() - start.getFullYear();
-    const months = end.getMonth() - start.getMonth();
-
-    return years * 12 + months + 1; // +1 to include the current month
-};
